cf50ece91ff3c9747d93e6ecc6808075559f2949
Vivek Pamulapati <vpamulap@gmail.com>
2011-11-19 10:35:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeeklyEats::Application.config.secret_token = 'e717a12df9ab4aa2a7f9ff881d1a65ae8185f493bdef1e6f1f945f98300922e69e8e04d48d2656fdd3b3a57d7c2dee74937a6f6802ebadc7403fc413398f8d0a'
