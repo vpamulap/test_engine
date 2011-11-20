329722436bf340c78ad288026c2bfc10a96ee536
Conor Gilsenan <conorgilsenan@gmail.com>
2011-11-19 21:40:12 -0500

    adds dashboard controller and recipe template

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ WeeklyEats::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  match 'dashboard/recipes' => 'dashboard#recipes', :as => :dashboard_recipes
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
