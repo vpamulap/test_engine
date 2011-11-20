f350440d8464c1db862382be3e8c6dfe6390689c
Conor Gilsenan <conorgilsenan@gmail.com>
2011-11-19 21:41:18 -0500

    adds the files I claimed to add even though I was in the wrong directory

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,19 @@
+class DashboardController < ApplicationController
+  def confirm_meals
+    # todo: implement logic to save the meals the user selected 
+  end
+
+  def recipes
+    # todo: choose X random meals and show them in the view
+    @recipes = Recipe.all
+    respond_to do |format|
+      format.html # recipe.html.erb
+      format.json { render json: @recipes }
+    end
+  end
+  
+  def change_recipe
+    # todo: returns a new random recipe template to replace one they rejected.
+    # Pick one that is not already shown on the page
+  end
+end
